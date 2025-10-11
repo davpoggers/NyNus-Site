25fafce04a4dacd304534b0e863737a8fc19e380
dav_poggers <d4v1dabatata@gmail.com>
2025-10-10 21:55:17 -0300

    Add files via upload
    Imagem da logo adicionada + "Verificação" de usuário feita com javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+    const emailUser = document.getElementById("email").value;
+    const senhaUser = document.getElementById("senha").value;
+
+    const emailVerif = "teste@gmail.com";
+    const senhaVerif = "123Teste";
+
+    if(emailUser == emailVerif && senhaUser == senhaVerif){
+        alert("Login efetuado com sucesso!")
+    } else{
+        alert("Email ou senha inválido(s)!")
+    }
+}
+            
